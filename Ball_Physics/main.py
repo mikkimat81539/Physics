6d09ef99273a85ce49e452ff604e0383fcc523f0
mikkimat81539 <mikkimat81539@gmail.com>
2026-07-14 04:48:18 +0100

    created ball object

--- Ball_Physics/main.py
+++ Ball_Physics/main.py
@@ -1,8 +1,25 @@
 import pygame
 
+# OBJECT CLASSES
+class Ball_Object:
+	def __init__(self, x_pos, y_pos, radius, color):
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		self.center = (self.x_pos, self.y_pos)
+		self.radius = radius
+		self.color = color
+
+	def drawObject(self, surface):
+		pygame.draw.circle(surface, self.color, self.center, self.radius)
+
+# SCREEN
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Bouncing Ball")
 
+# BALL
+ball = Ball_Object(100, 100, 10, "black")
+
+# GAME LOOP
 running = True
 
 while running:
@@ -11,6 +28,8 @@ while running:
 			running = False
 
 	screen.fill("white")
+
+	ball.drawObject(screen)
 
 	pygame.display.flip()
 
