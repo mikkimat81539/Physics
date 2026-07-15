8ec2c779e29660ab9b65913d586aa53b1612201c
mikkimat81539 <mikkimat81539@gmail.com>
2026-07-15 20:51:01 +0100

    ball bouncing along floor and wall

--- Ball_Physics/ball_challenges.md
+++ Ball_Physics/ball_challenges.md
@@ -1,6 +1,6 @@
 # Different Ball Physic simulataions
 - 1) Get a ball to bounce using spacebar -- DONE
-- 2) Get ball to bounce around floor and walls
+- 2) Get ball to bounce around floor and walls -- DONE
 - 3) Apply charge to a ball when spacebar is held
 - 4) Apply gravity settings
 - 5) Apply direction of where ball should bounce (show arrow)
--- Ball_Physics/ball_collision.py
+++ Ball_Physics/ball_collision.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 
 class Ball_Object:
@@ -9,11 +9,26 @@ class Ball_Object:
 		self.color = color
 		self.radius = radius
 
+		self.vel_x = 21
+		self.vel_y = 17
+
 	def drawObject(self, surface):
 		pygame.draw.circle(surface, self.color, self.center, self.radius)
 
 	def moveObject(self):
-		pass
+		self.center[0] += self.vel_x # x-axis movement
+		self.center[1] += self.vel_y # y-axis movement
+
+		self.collision()
+
+	def collision(self):
+		boundary_x, boundary_y = (500 - self.radius), (500 - self.radius)
+		
+		if self.center[0] >= boundary_x or self.center[0] <= 5:
+			self.vel_x = -self.vel_x
+
+		if self.center[1] >= boundary_y or self.center[1] <= 5:
+			self.vel_y = -self.vel_y
 
 pygame.init()
 
@@ -22,7 +37,7 @@ clock = pygame.time.Clock()
 
 
 # Ball Object
-ball = Ball_Object(250, 250, "white", 10)
+ball = Ball_Object(random.randint(10, 420), random.randint(10, 420), "white", 20)
 
 running = True
 
@@ -33,6 +48,8 @@ while running:
 
 	screen.fill("#c296ff")
 
+	# MOVEMENT
+	ball.moveObject()
 
 	# DRAW
 	ball.drawObject(screen)
