eae6047e49149e42e9ca1f564fb6bb0e7c3236d2
mikkimat81539 <mikkimat81539@gmail.com>
2026-07-21 01:38:24 +0100

    renamed file

--- Ball_Physics/curses_ball.py
+++ Ball_Physics/curses_ball.py
@@ -1,8 +1,4 @@
-# Create ball
-# Create and display a charge
-	# To display charge have a block increase in height as spacebar is held
-	# When spacebar is held velocity will increase (max 20)
-	# When spacebar is release the ball should move based on charged velocity 
+# Create ball -- DONE
 
 import curses
 
